2eb05e2d0468d836d09acea3653378b7af4d755f
Mahesh Hegde <46179734+mahesh-hegde@users.noreply.github.com>
2023-04-06 21:29:31 +0530

    Add dart example

---
+++ jar_lister_example/bin/jar_lister_example.dart
@@ -0,0 +1,5 @@
+import 'package:jar_lister_example/jar_lister_example.dart' as jar_lister_example;
+
+void main(List<String> arguments) {
+  print('Hello world: ${jar_lister_example.calculate()}!');
+}
